b7718c87284d784ac6286519624733a2eb749852
Nathan Lovato <nathan@gdquest.com>
2020-05-27 20:17:33 -0600

    Make sphere dissolve animation play with ui_accept, ease in

--- godot/Demos/Dissolve3D/DissolvingSphere.gd
+++ godot/Demos/Dissolve3D/DissolvingSphere.gd
@@ -5,13 +5,25 @@ onready var material := get_surface_material(0)
 
 
 func _ready() -> void:
+	material.set_shader_param("dissolve_amount", 0.0)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		dissolve()
+
+
+func dissolve():
+	visible = true
 	tween.interpolate_method(
-		self, "on_dissolve_param", 0, 1, 3.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+		self, "animate_dissolve", 0, 1, 3.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
 	)
 	tween.start()
-	yield(tween, "tween_all_completed")
+
+
+func animate_dissolve(progress: float) -> void:
+	material.set_shader_param("dissolve_amount", ease(progress, 0.4))
+
+
+func _on_Tween_tween_all_completed() -> void:
 	visible = false
-
-
-func on_dissolve_param(value: float) -> void:
-	material.set_shader_param("dissolve_amount", value)
