0098d29a1fa4a2a75bdf16fb6c2b03f6866009f6
Francois Belair <razoric480@gmail.com>
2020-05-28 13:00:34 -0400

    Refactor 2D demos to use inherited scene with BG

--- godot/Demos/Dissolve2D/Dissolve2DControls.gd
+++ godot/Demos/Dissolve2D/Dissolve2DControls.gd
@@ -1,10 +1,11 @@
 tool
-extends Node2D
+extends Node
 
 export (float, 0, 1) var debug_dissolve_control := 0.0 setget _set_debug_control
 
-onready var mask := $MaskView/RobiMask
-onready var robi := $Robi
+onready var mask := get_parent().get_node("MaskView/RobiMask")
+onready var robi := get_parent().get_node("Robi")
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
@@ -14,6 +15,9 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _set_debug_control(value: float) -> void:
 	debug_dissolve_control = value
+	if not is_inside_tree():
+		yield(self, "ready")
+
 	if Engine.editor_hint:
 		mask.debug_dissolve_control = value
 		robi.debug_dissolve_control = value
--- godot/Shaders/xray_mask.shader
+++ godot/Shaders/xray_mask.shader
@@ -5,6 +5,7 @@ uniform sampler2D mask_viewport;
 uniform float dim_main_view : hint_range(0, 1) = 0.2;
 
 void fragment() {
+	vec4 background_color = texture(SCREEN_TEXTURE, SCREEN_UV);
 	vec4 main_color = texture(TEXTURE, UV);
 	float mask_color = texture(mask_viewport, UV).r;
 	vec4 alternate_color = texture(alternate_viewport, UV);
@@ -13,5 +14,5 @@ void fragment() {
 	out_color += alternate_color * mask_color;
 	vec4 dim_color = vec4(vec3(dim_main_view), 0.0) * (1.0 - mask_color);
 
-	COLOR = clamp(out_color - dim_color, 0, 1);
+	COLOR = clamp(out_color - dim_color + background_color * (1.0 - main_color.a), 0, 1);
 }
