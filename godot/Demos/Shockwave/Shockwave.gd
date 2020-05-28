a5fdae867769c531b53dc3ffa1d831926b9b75cd
Francois Belair <razoric480@gmail.com>
2020-05-28 13:12:17 -0400

    Shockwave demo now updates mask preview
    Fixes #4

--- godot/Demos/Shockwave/Shockwave.gd
+++ godot/Demos/Shockwave/Shockwave.gd
@@ -3,7 +3,10 @@ extends ViewportContainer
 
 export var shockwave_duration := 1.0
 
+var torus_radius := -0.25 setget _set_torus_radius
+
 onready var tween := $Tween
+onready var preview_mask := get_parent().get_node("MaskPreview/Mask")
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -12,11 +15,19 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func blast() -> void:
-	tween.interpolate_method(
-		self, "shockwave", -0.1, 2.0, shockwave_duration, Tween.TRANS_LINEAR, Tween.EASE_OUT
+	tween.interpolate_property(
+		self, "torus_radius", -0.25, 2.0, shockwave_duration, Tween.TRANS_LINEAR, Tween.EASE_OUT
 	)
 	tween.start()
 
 
-func shockwave(value: float) -> void:
-	material.set_shader_param("torus_radius", value)
+func _set_torus_radius(value: float) -> void:
+	torus_radius = value
+	if not is_inside_tree():
+		yield(self, "ready")
+
+	synchronize_materials("torus_radius", torus_radius)
+
+func synchronize_materials(parameter_name: String, parameter_value) -> void:
+	material.set_shader_param(parameter_name, parameter_value)
+	preview_mask.material.set_shader_param(parameter_name, parameter_value)
