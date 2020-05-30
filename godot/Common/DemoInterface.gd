8074ab95e6f1656757de3a6eeaeb5d0f7c4b8730
Nathan Lovato <nathan@gdquest.com>
2020-05-30 17:17:02 -0600

    Simplify DemoInterface
    Add a property on the root node to control the text

---
+++ godot/Common/DemoInterface.gd
@@ -0,0 +1,13 @@
+tool
+extends Control
+
+export var text_bbcode := "" setget set_text_bbcode
+
+onready var rich_text_label := $RichTextLabel
+
+
+func set_text_bbcode(value: String) -> void:
+	text_bbcode = value
+	if not rich_text_label:
+		yield(self, "ready")
+	rich_text_label.bbcode_text = value
