76c742a78bc808c8d5642a13e68482393cfddf04
Nathan Lovato <nathan@gdquest.com>
2020-05-30 20:26:40 -0600

    Add 2D water demo
    Closes #6

---
+++ godot/Demos/Water2D/Water2D.gd
@@ -0,0 +1,15 @@
+tool
+extends Sprite
+
+
+func _ready():
+	update_shader_aspect_ratio()
+	get_node("/root").transparent_bg = true
+
+
+func _on_item_rect_changed():
+	update_shader_aspect_ratio()
+
+
+func update_shader_aspect_ratio():
+	material.set_shader_param("aspect_ratio", scale.y/scale.x)
