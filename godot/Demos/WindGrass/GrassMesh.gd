1f0d2aa168ec40dfda92828b6afbf010b7f82dd8
nonunknown <58845030+nonunknown@users.noreply.github.com>
2020-08-09 19:31:05 +0100

    Add random outside circle option to grass (#17)
    * Add random outside circle option to wind grass
    
    Added a option to generate random grasses outside a circle radius.

--- godot/Demos/WindGrass/GrassMesh.gd
+++ godot/Demos/WindGrass/GrassMesh.gd
@@ -1,18 +1,37 @@
 tool
 extends MultiMeshInstance
 
-export var extents := Vector2.ONE
+export(Vector2) var extents := Vector2.ONE
+export(bool) var spawn_outside_circle := false
+export(float) var radius := 12.0
 
+func _enter_tree():
+	connect("visibility_changed",self,"_on_WindGrass_visibility_changed")
 
 func _ready() -> void:
 	var rng := RandomNumberGenerator.new()
 	rng.randomize()
+	
+	var theta := 0
+	var increase := 1
+	var center := get_parent().global_transform.origin
+	
 	for i in multimesh.instance_count:
 		var transform := Transform().rotated(Vector3.UP, rng.randf_range(-PI/2, PI/2))
-		
-		var x := rng.randf_range(-extents.x, extents.x)
-		var z := rng.randf_range(-extents.y, extents.y)
-		transform.origin = Vector3(x, 0, z)
+		var x:float
+		var z:float
+		if (!spawn_outside_circle):
+			x := rng.randf_range(-extents.x, extents.x)
+			z := rng.randf_range(-extents.y, extents.y)
+		else:
+			x := center.x + (radius + rng.randf_range(0,extents.x) ) * cos(theta)
+			z := center.z + (radius + rng.randf_range(0,extents.y) ) * sin(theta)
+			theta += increase
+		transform.origin := Vector3(x, 0, z)
 
 		multimesh.set_instance_transform(i, transform)
 		multimesh.set_instance_custom_data(i, Color(rng.randf(), 0, rng.randf(), 0))
+
+func _on_WindGrass_visibility_changed():
+	if visible:
+		_ready()
