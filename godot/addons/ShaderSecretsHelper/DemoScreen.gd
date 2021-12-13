edaca74cc4109f2eb905221560628606d3b52df3
Bram <bramreth@gmail.com>
2021-12-13 15:51:55 +0000

    feat: add csv file, and class for handling showing demo labels

---
+++ godot/addons/ShaderSecretsHelper/DemoScreen.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+class_name DemoScreen
+
+onready var _demo_interface: Control = $DemoText
+
+func _ready() -> void:
+	var translation_key: String = name.to_lower()
+	_demo_interface.set_text_bbcode(tr(translation_key))
+	if _demo_interface.text_bbcode == translation_key:
+		print_debug(
+			"Missing translation key {} for demo {}".format(
+				[translation_key, name], "{}"
+			)
+		)
