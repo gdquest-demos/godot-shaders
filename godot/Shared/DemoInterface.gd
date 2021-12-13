5035e2e0ef6ffc482b4b28a43fbcee463f176fbe
Bram <bramreth@gmail.com>
2021-12-13 15:59:29 +0000

    chore: update demo label font size and margin settings

--- godot/Shared/DemoInterface.gd
+++ godot/Shared/DemoInterface.gd
@@ -3,7 +3,7 @@ extends Control
 
 export (String, MULTILINE) var text_bbcode := "" setget set_text_bbcode
 
-onready var _rich_text_label: RichTextLabel = $RichTextLabel
+onready var _rich_text_label: RichTextLabel = $MarginContainer/RichTextLabel
 
 
 func set_text_bbcode(value: String) -> void:
