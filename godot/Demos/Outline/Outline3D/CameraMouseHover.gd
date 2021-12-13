049ce0c1f003f2038e80ffeffb74767abe204985
Bram <bramreth@gmail.com>
2021-12-13 14:35:29 +0000

    refactor:  fix 3d hover and demo screen visibility

--- godot/Demos/Outline/Outline3D/CameraMouseHover.gd
+++ godot/Demos/Outline/Outline3D/CameraMouseHover.gd
@@ -4,11 +4,11 @@ const MOUSE_RANGE := 10000
 var last_target: Spatial
 
 onready var ray: RayCast = $RayCast
-
+onready var view := get_tree().root
 
 func _physics_process(_delta: float) -> void:
 	# Cast a ray straight out from the camera through the current mouse position
-	var mouse_position := get_viewport().get_mouse_position()
+	var mouse_position := view.get_mouse_position()
 	ray.cast_to = project_local_ray_normal(mouse_position) * MOUSE_RANGE
 	ray.force_raycast_update()
 	
