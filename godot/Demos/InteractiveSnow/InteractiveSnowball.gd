150299fc7572b317c455b19ad6456f81776c6262
tetrapod <145553014+tetrapod00@users.noreply.github.com>
2024-09-18 00:06:47 -0700

    feat: Update snow shader to godot 4.3
    * Convert snow shader
    
    - Update shader
    - Clamp snowball position
    
    * chore: proofread and minor renames snow shader
    
    ---------
    
    Co-authored-by: Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>

--- godot/Demos/InteractiveSnow/InteractiveSnowball.gd
+++ godot/Demos/InteractiveSnow/InteractiveSnowball.gd
@@ -1,9 +1,9 @@
-# ANCHOR: all
 extends MeshInstance3D
 
 @export var move_speed := 5.0
 @export var rotate_speed := PI
 
+@export var snow_size := Vector2(1.0, 1.0)
 
 func _process(delta: float) -> void:
 	var move_dir = Vector2()
@@ -11,4 +11,7 @@ func _process(delta: float) -> void:
 	move_dir.y = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
 	rotate_y(move_dir.x * rotate_speed * delta)
 	global_transform.origin += -global_transform.basis.z * move_speed * delta * move_dir.y
-# END: all
+	global_transform.origin = Vector3(
+			clamp(global_transform.origin.x, -snow_size.x, snow_size.x), 
+			global_transform.origin.y, 
+			clamp(global_transform.origin.z, -snow_size.y, snow_size.y))
