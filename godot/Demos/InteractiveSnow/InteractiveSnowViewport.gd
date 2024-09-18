9f358b465665bc54badd2333ed1bd48dddd8fe83
tetrapod00 <145553014+tetrapod00@users.noreply.github.com>
2024-09-17 19:02:54 -0700

    Convert snow shader
    - Update shader
    - Clamp snowball position

--- godot/Demos/InteractiveSnow/InteractiveSnowViewport.gd
+++ godot/Demos/InteractiveSnow/InteractiveSnowViewport.gd
@@ -1,19 +1,18 @@
-# ANCHOR: all
-# ANCHOR: vars
 extends SubViewport
 
+#region vars
 @export var interactive_actor_path := NodePath("../InteractiveSnowball")
 @export var snow_plane_path := NodePath("../SnowPlane")
 
 @onready var snow_size: Vector2 = get_node(snow_plane_path).mesh.size / 2
 @onready var _player: MeshInstance3D = get_node(interactive_actor_path)
 @onready var _footprint: Sprite2D = $Sprite2D
-# END: vars
-# ANCHOR: footprint_positioning
+#endregion vars
+
+#region Callbacks
 func _ready() -> void:
-	# Enable zoom filtering, so our viewport texture isn't pixellated.
-	get_texture().set_flags(Texture2D.FLAG_FILTER)
 	_footprint.position = get_world_to_viewport_position_verbose(_player.global_transform.origin)
+	_player.snow_size = snow_size
 
 
 func _process(_delta: float) -> void:
@@ -24,18 +23,16 @@ func _process(_delta: float) -> void:
 	_footprint.modulate.a = _footprint.position.distance_to(player_pos)
 	# Update the footprints position.
 	_footprint.position = player_pos
-# END: footprint_positioning
-# ANCHOR: 3d-to-2d
+#endregion Callbacks
+
+#region 3d-to-2d
 func get_world_to_viewport_position(world_position: Vector3) -> Vector2:
 	# Convert from (-8, 8) 3D space to (0, 128) 2D space. y = (x + 8) * 8
 	return (Vector2(world_position.x, world_position.z) + snow_size) * snow_size
-# END: 3d-to-2d
 
-# ANCHOR: 3d-to-2d-verbose
 func get_world_to_viewport_position_verbose(world_position: Vector3) -> Vector2:
 	# Convert from (-8, 8) 3D space to (0, 128) 2D space.
 	var x_component = remap(world_position.x, -snow_size.x, snow_size.x, 0, size.x)
 	var y_component = remap(world_position.z, -snow_size.y, snow_size.y, 0, size.y)
 	return (Vector2(x_component, y_component))
-# END: 3d-to-2d-verbose
-# END: all
+#endregion 3d-to-2d
