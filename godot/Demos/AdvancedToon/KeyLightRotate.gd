42c1ec8d933e967aba806af3fe566890ac8136d9
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-10-20 22:18:35 +0200

    remove advanced toon demo with deferred shading

--- godot/Demos/AdvancedToon/KeyLightRotate.gd
+++
@@ -1,5 +0,0 @@
-extends Node3D
-
-
-func _process(delta: float) -> void:
-	rotate_y(deg_to_rad(45) * delta)
--- godot/addons/gdquest.toon-controller/Tools/ToonViewportContainer.gd
+++
@@ -1,7 +0,0 @@
-@tool
-class_name ToonViewportContainer
-extends SubViewportContainer
-
-
-func _enter_tree() -> void:
-	stretch = not Engine.is_editor_hint()
